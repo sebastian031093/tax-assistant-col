fb0dc1db9526d4608abd2fad3c609736528a4de6
Sebastian Manco G <sebasmg1393@gmail.com>
2026-08-09 14:27:06 -0500

    feat(core): add chakra providers

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,23 @@
 import { useState } from 'react';
 import './App.css';
 
+// type TaxYear = {
+//   year: number;
+//   status: 'draft' | 'completed';
+// };
+
 function App() {
   const [appName, setAppName] = useState('Sebas Dian app');
+
+  //TODO: que es la verificacion estatica de typeScript
+  //TypeScript analiza el codigo antes de ejecutarlo para detectar operacion incompatibles
+  // const salary: number = 6_2234_32423;
+  // salary.toUppercase();
+
+  // const TaxtYear: TaxYear = {
+  //   year: 'djjdsgjfsdag',
+  //   status: 'pendding',
+  // };
 
   return (
     <div>
