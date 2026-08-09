61f811a30d9e34fa589eab261f2bb568d304a71f
Sebastian Manco G <sebasmg1393@gmail.com>
2026-08-09 14:47:51 -0500

    feat(core): add chakra components

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider value={defaultSystem}>
       <App />
     </ChakraProvider>
   </StrictMode>,
